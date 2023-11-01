27c7be775abb109754d95dc201eca35075fa1ddf
Tokarev Kirill <kirill.2005.tokarev@mail.ru>
2023-11-01 17:12:59 +0500

    laba12

--- ServicePagexaml.xaml.cs
+++ ServicePagexaml.xaml.cs
@@ -20,11 +20,21 @@ namespace TokarevGlazki
     /// </summary>
     public partial class ServicePagexaml : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+
+
         public ServicePagexaml()
         {
             InitializeComponent();
             var currentAgent = Tokarev_GlazkiSaveEntities.GetContext().Agent.ToList();
             AgentListView.ItemsSource = currentAgent;
+
         }
         private void UpdatePage()
         {
@@ -73,6 +83,9 @@ namespace TokarevGlazki
                 currentGlazki = currentGlazki.OrderByDescending(p => p.Priority).ToList();
             }
             AgentListView.ItemsSource = currentGlazki;
+            TableList = currentGlazki;
+            ChangePage(0, 0);
+          
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -123,6 +136,100 @@ namespace TokarevGlazki
         private void Filtraciya_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdatePage();
+        }
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            Boolean Ifupdate = true;
+            int min;
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CountPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+                
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                AgentListView.ItemsSource = CurrentPageList;
+                AgentListView.Items.Refresh();
+            }
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
         }
     }
 }
